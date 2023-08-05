20267ae0405ee451743cc51cbd922217a608ca1f
Chris Horton <cphorton@gmail.com>
2023-08-05 10:59:45 +1000

    Added harpoon

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,5 +39,16 @@ return {
         hide_numbers = true,
         open_mapping = [[<c-t>]]
     }, config = true },
-    {'ThePrimeagen/harpoon', config = true}
+    {'ThePrimeagen/harpoon', config = true,
+        keys = {
+      { "<leader>hm", function() require('harpoon.mark').add_file() end, desc = "Harpoon Mark" },
+      { "<leader>hh", function() require('harpoon.ui').toggle_quick_menu() end, desc = "Harpoon UI" },
+      { "<leader>h1", function() require("harpoon.ui").nav_file(1) end, desc = "Harpoon File 1" },
+      { "<leader>h2", function() require('harpoon.ui').nav_file(2) end, desc = "Harpoon File 2" },
+      { "<leader>h3", function() require('harpoon.ui').nav_file(3) end, desc = "Harpoon File 3" },
+      { "<leader>h4", function() require('harpoon.ui').nav_file(4) end, desc = "Harpoon File 4" },
+      { "<leader>hj", function() require("harpoon.ui").nav_next() end, desc = "Harpoon Next File" },
+      { "<leader>hn", function() require("harpoon.ui").nav_prev() end, desc = "Harpoon Previous File" },
+    }
+  }
 }
