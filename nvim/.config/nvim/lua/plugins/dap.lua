3845c45c267df02bad5797461c3884e8bb009776
Chris Horton <cphorton@gmail.com>
2026-08-09 20:41:45 +1000

    fix(dap): stop nvim-dap-ui auto-opening when a debug session starts
    Removes the event_initialized listener that called dapui.open(). Manual
    toggle (<leader>du) and auto-close on session terminate/exit are unchanged.

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -94,9 +94,6 @@ return {
             local dap = require("dap")
             local dapui = require("dapui")
             dapui.setup(opts)
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open({})
-            end
             dap.listeners.before.event_terminated["dapui_config"] = function()
                 dapui.close({})
             end
