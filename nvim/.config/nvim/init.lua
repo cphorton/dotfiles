7be0f725b823b3f352194db8759d071657e0fa0e
Chris Horton <cphorton@gmail.com>
2026-08-28 06:29:37 +1000

    chore: remove dead autocmd.lua boilerplate
    100% commented-out LazyVim-starter example, never required (the
    require("config.autocmds") line that would load it was itself already
    commented out). Removing both the file and the dead require.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,6 @@ require("config.lazy")
 vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()
-        --require("config.autocmds")
         require("config.keymaps")
 
         require("snacks.dashboard")
